18c943070b165213ad1aa248a554220bb82774a8
Ben Obee <benobee@gmail.com>
2015-08-02 18:09:49 -0700

    adding files

---
+++ client/router.js
@@ -0,0 +1,13 @@
+Router.configure({
+	layoutTemplate:'layout'
+});
+
+Router.route('/', function () {
+  this.render('home');
+});
+
+Router.map(function(){
+	this.route('home', {path:'/home'});
+	this.route('profile', {path:'/profile'});
+});
+
